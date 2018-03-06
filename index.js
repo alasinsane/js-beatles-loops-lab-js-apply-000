516ebba5cdadfd2f12bbc335e1a49ff26c8f1dd9
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-03-06 00:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(musicians, instruments) {
   var n = [];
   for (i = 0; i < musicians.length; i++) {
     for (j = 0; j < instruments.length; j++) {
-     n = musicians[i] + " plays " instruments[j];
+     n = musicians[i] + " plays " + instruments[j];
     }
   }
     return n;
