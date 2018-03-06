98a922628fa566d97ac47a524d5234e9b733ec51
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-03-06 00:07:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicians, instruments) {
+  musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  var n = [];
+  for (i = 0; i < musicians.length; i++) {
+    for (j = 0; j < instruments.length; j++) {
+     n = musicians[i] + " plays " instruments[j];
+    }
+  }
+    return n;
+}
+
+function iLoveTheBeatles(number) {
+  for (i = 0; i )
+}
